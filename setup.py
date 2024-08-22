1da9138fd77cc4b7e0e2f53574007510ae0fdc7a
bbalouki <bbalouki@outlook.com>
2024-08-22 18:02:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,86 @@
 from setuptools import setup
-
 import sys
 
 if sys.version_info < (3,9):
     sys.exit("Only Python 3.9 and greater is supported") 
 
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Simplified Investment & Trading Toolkit'
+
+# Setting up
 setup(
-    name='bbstrader',
-    version='1.0',
-    packages=['bbstrader'],
-    url='https://github.com/bbalouki/BBSTrade',
-    license='The MIT License (MIT)',
+    name="bbstrader",
+    version=VERSION,
     author='Bertin Balouki SIMYELI',
-    author_email='bbalouki@outlook.com',
-    description='Simplified Investment & Trading Toolkit'
+    url='https://github.com/bbalouki/bbstrade',
+    license='The MIT License (MIT)',
+    author_email='<bbalouki@outlook.com>',
+    maintainer='Bertin Balouki SIMYELI',
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=[
+        "bbstrader", 
+        "bbstrader.btengine", 
+        "bbstrader.metatrader", 
+        "bbstrader.trading"
+    ],
+    install_requires=[
+        "pandas",
+        "numpy==1.26.4",
+        "yfinance",
+        "scipy",
+        "hmmlearn",
+        "pmdarima",
+        "arch",
+        "hurst",
+        "seaborn",
+        "statsmodels",
+        "matplotlib",
+        "filterpy",
+        "pytest",
+        "sphinx",
+        "sphinx-rtd-theme",
+        "CurrencyConverter",
+        "Metatrader5"
+        ],
+    keywords=[
+        "Finance",
+        "Toolkit",
+        "Financial",
+        "Analysis",
+        "Fundamental",
+        "Quantitative",
+        "Database",
+        "Equities",
+        "Currencies",
+        "Economics",
+        "ETFs",
+        "Funds",
+        "Indices",
+        "Moneymarkets",
+        "Commodities",
+        "Futures",
+        "CFDs",
+        "Derivatives",
+        "Trading",
+        "Investing",
+        "Portfolio",
+        "Optimization",
+        "Performance",
+        ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ]
 )
