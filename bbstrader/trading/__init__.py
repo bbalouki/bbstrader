d335d62906bb13ae69186447293e96049ffdfe3a
bbalouki <bbalouki@outlook.com>
2024-09-06 20:34:06 +0000

    Add new module to offer an unified way for building strategies for backtesting and live execution

--- bbstrader/trading/__init__.py
+++ bbstrader/trading/__init__.py
@@ -1,4 +1,11 @@
 """
 Trading strategies execution module
+
+This module is responsible for executing trading strategies. 
+It provides a framework for executing trading strategies and managing the trading process. 
+The module is designed to be flexible and extensible, allowing users to define their own trading 
+strategies and customize the trading process.
+
 """
-from bbstrader.trading.run import *
+from bbstrader.trading.execution import ExecutionEngine
+from bbstrader.trading.strategies import *
