d9ba945602684aadfb549c6b96dce09014db4690
bbalouki <bbalouki@outlook.com>
2025-01-14 12:22:34 +0000

    Update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with io.open(path.join(here, "README.md"), encoding="utf-8") as f:
 with io.open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
     REQUIREMENTS = [line.rstrip() for line in f]
 
-VERSION = "0.2.02"
+VERSION = "0.2.04"
 DESCRIPTION = "Simplified Investment & Trading Toolkit"
 
 KEYWORDS = [
@@ -89,7 +89,6 @@ setup(
     install_requires=REQUIREMENTS,
     extras_require={
         "MT5": ["MetaTrader5"],
-        "TA": ["ta-lib"],
         # 'zipline': ['zipline'],
         # 'cerebro': ['backtrader'],
     },
