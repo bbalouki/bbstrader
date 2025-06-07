ba941e03912e11847ae172b0275ce2d96b91c4a7
bbalouki <bbalouki@outlook.com>
2025-06-07 11:51:14 +0000

    Add new CLI argument for news feed

--- bbstrader/__main__.py
+++ bbstrader/__main__.py
@@ -1,15 +1,29 @@
 import argparse
 import sys
+from enum import Enum
 
 import pyfiglet
 from colorama import Fore
 
 from bbstrader.btengine.scripts import backtest
+from bbstrader.core.scripts import send_news_feed
 from bbstrader.metatrader.scripts import copy_trades
 from bbstrader.trading.scripts import execute_strategy
 
-DESCRIPTION = "BBSTRADER"
-USAGE_TEXT = """
+
+class Module(Enum):
+    COPIER = "copier"
+    BACKTEST = "backtest"
+    EXECUTION = "execution"
+    NEWS_FEED = "news_feed"
+
+
+FONT = pyfiglet.figlet_format("BBSTRADER", font="big")
+
+
+def main():
+    DESCRIPTION = "BBSTRADER"
+    USAGE_TEXT = """
     Usage:
         python -m bbstrader --run <module> [options]
 
@@ -17,14 +31,10 @@ USAGE_TEXT = """
         copier: Copy trades from one MetaTrader account to another or multiple accounts
         backtest: Backtest a strategy, see bbstrader.btengine.backtest.run_backtest
         execution: Execute a strategy, see bbstrader.trading.execution.Mt5ExecutionEngine
+        news_feed: Send news feed from Coindesk to Telegram channel
     
     python -m bbstrader --run <module> --help for more information on the module
-"""
-
-FONT = pyfiglet.figlet_format("BBSTRADER", font="big")
-
-
-def main():
+    """
     print(Fore.BLUE + FONT)
     print(Fore.WHITE + "")
     parser = argparse.ArgumentParser(
@@ -39,12 +49,19 @@ def main():
     if ("-h" in sys.argv or "--help" in sys.argv) and args.run is None:
         print(Fore.WHITE + USAGE_TEXT)
         sys.exit(0)
-    if args.run == "copier":
-        copy_trades(unknown)
-    elif args.run == "backtest":
-        backtest(unknown)
-    elif args.run == "execution":
-        execute_strategy(unknown)
+
+    match args.run:
+        case Module.COPIER.value:
+            copy_trades(unknown)
+        case Module.BACKTEST.value:
+            backtest(unknown)
+        case Module.EXECUTION.value:
+            execute_strategy(unknown)
+        case Module.NEWS_FEED.value:
+            send_news_feed(unknown)
+        case _:
+            print(Fore.RED + f"Unknown module: {args.run}")
+            sys.exit(1)
 
 
 if __name__ == "__main__":
