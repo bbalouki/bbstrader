bf2381bc4345d811ea733ec549e976c4190eebec
bbalouki <bbalouki@outlook.com>
2025-09-13 15:24:07 +0000

    updates

--- bbstrader/compat.py
+++ bbstrader/compat.py
@@ -2,18 +2,26 @@ import platform
 import sys
 
 
-def setup_mock_metatrader():
-    """Mock MetaTrader5 on Linux and MacOS to prevent import errors."""
+def setup_mock_modules():
+    """Mock some modules not available on some OS to prevent import errors."""
+    from unittest.mock import MagicMock
+
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return MagicMock()
+        
+    MOCK_MODULES = []
+
+    # Mock Metatrader5 on Linux and MacOS
     if platform.system() != "Windows":
-        from unittest.mock import MagicMock
+        MOCK_MODULES.append("MetaTrader5")
 
-        class Mock(MagicMock):
-            @classmethod
-            def __getattr__(cls, name):
-                return MagicMock()
+    # Mock posix On windows
+    if platform.system() == "Windows":
+        MOCK_MODULES.append("posix")
 
-        MOCK_MODULES = ["MetaTrader5"]
-        sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
-setup_mock_metatrader()
+setup_mock_modules()
